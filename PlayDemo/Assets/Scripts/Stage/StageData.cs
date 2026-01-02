9956b30b582b1d0db476e3c0e6a969f3d2fc7f21
imanoony <ayoonchoi1228@gmail.com>
2026-01-03 03:12:40 +0900

    [feat] stage editor
    사용 방법은 PR에서

--- PlayDemo/Assets/Scripts/Stage/StageData.cs
+++ PlayDemo/Assets/Scripts/Stage/StageData.cs
@@ -6,17 +6,19 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor;
 
 [CreateAssetMenu(fileName = "Stage", menuName = "Scriptable Objects/Stage")]
 public class StageData : ScriptableObject
 {
     public string id;               // 구분용 이름 (integer 대신)
     public Int2 player;             // 플레이어의 시작 좌표
-    public List<Int2> enemies;      // 적들의 시작 좌표
+    public List<Int2> enemies = new();      // 적들의 시작 좌표
     
 #region Terrain
-    public List<Int2> grounds;      // 땅 타일 좌표
-    public List<Int2> walls;        // 벽 타일 좌표
+    public List<Int2> grounds = new();      // 땅 타일 좌표
+    public List<Int2> walls = new();        // 벽 타일 좌표
 #endregion
 
 }
@@ -29,4 +31,114 @@ public class Int2
 {
     public int x;
     public int y;
+}
+
+// 스테이지 에디터를 위한 정적 함수 모음
+// 현재 에디터 상황을 저장해 스크립터블 오브젝트로 변환 등
+// save: 에디터 상황 -> 스크립터블 오브젝트 (직렬화)
+// load: 스크립터블 오브젝트 -> 에디터 상황 (역직렬화)
+// TODO: 하드한 prefab, tilebase 소프트하게 바꾸기.
+// TODO: 비 tilemap 오브젝트의 위치 저장을 조금 더 정밀하게.
+public static class StageSerializer
+{
+#if UNITY_EDITOR
+    // grid (terrian), player 위치 정보, enemy 위치 정보를
+    // 받아 ScriptableObject 형태 (stage) 로 저장한다.
+    public static void Save(
+        Grid grid,
+        Transform player,
+        Transform enemy, 
+        StageData stage
+    )
+    {
+        int i;
+        Vector3Int entityPos;
+        Tilemap tilemap;
+        TileBase tile;
+
+        if (player.childCount != 1) 
+        {
+            Debug.LogError("StageSerializer.Save(): invalid player count");
+            return;
+        }
+
+        stage.grounds.Clear();
+        stage.walls.Clear();
+        for (i = 0; i < grid.transform.childCount; i++)
+        {
+            tilemap = grid.transform.GetChild(i).GetComponent<Tilemap>();
+            foreach (Vector3Int pos in tilemap.cellBounds.allPositionsWithin)
+            {
+                tile = tilemap.GetTile(pos);
+                if (tile == null) continue;
+                if (tilemap.name.ToLower().Contains("ground"))
+                    stage.grounds.Add(new Int2 { x = pos.x, y = pos.y });
+                else 
+                    stage.walls.Add(new Int2 { x = pos.x, y = pos.y });
+            }
+        }
+
+        entityPos = grid.LocalToCell(player.GetChild(0).position);
+        stage.player = new Int2 { x = entityPos.x, y = entityPos.y };
+
+        stage.enemies.Clear();
+        for (i = 0; i < enemy.childCount; i++)
+        {
+            entityPos = grid.LocalToCell(enemy.GetChild(i).position);
+            stage.enemies.Add(new Int2 { x = entityPos.x, y = entityPos.y });
+        }
+
+        EditorUtility.SetDirty(stage);
+        AssetDatabase.SaveAssets();
+    }
+
+    // ScriptableObject를 받아 현재 에디터에
+    // grid (terrian), player, enemy 를 배치한다.
+    // 인자가 더럽지만 일단 테스트용으로... 이후에 고치겠지 뭐~
+    public static void Load(
+        StageData stage,
+        Grid grid,
+        Transform player,
+        Transform enemy,
+        TileBase[] tiles,
+        GameObject playerPrefab,
+        GameObject enemyPrefab
+    )
+    {
+        int i;
+        GameObject entity;
+        Tilemap groundTM = grid.transform.Find("Ground").GetComponent<Tilemap>();
+        Tilemap wallTM = grid.transform.Find("Wall").GetComponent<Tilemap>();
+
+        groundTM.ClearAllTiles();
+        wallTM.ClearAllTiles();
+        if (player.childCount > 0)
+            Undo.DestroyObjectImmediate(player.GetChild(0).gameObject);
+        for (i = enemy.childCount - 1; i >= 0; i--)
+            Undo.DestroyObjectImmediate(enemy.GetChild(i).gameObject);
+
+        // 땅 타일 배치하기
+        foreach (Int2 pos in stage.grounds)
+            groundTM.SetTile(new Vector3Int(pos.x, pos.y), tiles[0]);
+
+        // 벽 타일 배치하기
+        foreach (Int2 pos in stage.walls)
+            wallTM.SetTile(new Vector3Int(pos.x, pos.y), tiles[1]);
+
+        // 적들 스폰하기
+        foreach (Int2 pos in stage.enemies)
+        {
+            entity = (GameObject)PrefabUtility.InstantiatePrefab(enemyPrefab);
+            entity.transform.position = grid.CellToLocal(new Vector3Int(pos.x, pos.y));
+            entity.transform.SetParent(enemy);
+        }
+
+        // 플레이어 스폰하기
+        entity = (GameObject)PrefabUtility.InstantiatePrefab(playerPrefab);
+        entity.transform.position = grid.CellToLocal(new Vector3Int(stage.player.x, stage.player.y));
+        entity.transform.SetParent(player);
+    }
+
+    // TODO: 화면 clear 함수 만들기
+#endif
 }
--- PlayDemo/Assets/Scripts/Stage/StagePlacer.cs
+++ PlayDemo/Assets/Scripts/Stage/StagePlacer.cs
@@ -1,6 +1,7 @@
 // StageData 객체 바탕으로 씬에 Stage를 배치한다.
 // (0,0) 타일맵을 좌측 하단 경계로 생각하고 배치해서
 // 카메라의 적절한 조정이 필요하다.
+// TODO: 스테이지 배치에 맞게 카메라 조정하기.
 
 using System.Collections.Generic;
 using UnityEngine;
