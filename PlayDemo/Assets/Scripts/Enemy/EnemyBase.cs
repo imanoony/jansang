043ae637cad7dd88e2a50a9aba8105fc83613dbd
Jeon, Minseo <minseo4565@snu.ac.kr>
2026-01-12 17:51:03 +0900

    [feat] create enemy base class

---
+++ PlayDemo/Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,102 @@
+using System;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    #region  parameters
+
+    [Header("Ground Check")]
+    public LayerMask groundLayer;
+    public float groundCheckDistance = 0.05f;
+    
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 5f;
+
+    #endregion
+    
+    
+    #region components
+
+    protected Animator animator;
+    private Collider2D col;
+    private Rigidbody2D rb;
+    
+    #endregion
+    
+    #region status
+
+    protected enum State
+    {
+        Idle,
+        Alert,
+        Combat
+    }
+    
+    protected State currentState = State.Idle;
+    
+    private bool isGrounded;
+    private int moveDirection = 0; // -1 for left, 1 for right, and 0 for idle
+    
+    #endregion
+    
+    
+    protected virtual void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        col = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        CheckGround();
+        Move();
+    }
+
+    protected virtual void Update()
+    {
+         //mola
+    }
+
+    private void CheckGround()
+    {
+        Bounds bounds = col.bounds;
+
+        RaycastHit2D hit = Physics2D.BoxCast(
+            bounds.center,
+            bounds.size,
+            0f,
+            Vector2.down,
+            groundCheckDistance,
+            groundLayer
+        );
+
+        // 노멀 체크 (옆면/벽 배제)
+        isGrounded = hit.collider != null && hit.normal.y > 0.7f;
+    }
+    
+    private void Move()
+    {
+        rb.linearVelocity = new Vector2(moveDirection * moveSpeed, rb.linearVelocity.y);
+    }
+
+    protected void ChangeDirection(int direction)
+    {
+        moveDirection = direction;
+    }
+    
+    protected void TryJump(float jumpForce)
+    {
+        if (jumpForce < 0) return;
+        
+        if (isGrounded)
+        {
+            Jump(jumpForce);
+        }
+    }
+
+    private void Jump(float jumpForce)
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+}
