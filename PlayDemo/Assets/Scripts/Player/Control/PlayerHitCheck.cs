e2249cc2bf806841d619d04f5fa544d792e8dce5
Minseo Jeon <minseo4565@snu.ac.kr>
2026-02-14 21:26:13 +0900

    [feat] player hit fx

--- PlayDemo/Assets/Scripts/Player/Control/PlayerHitCheck.cs
+++ PlayDemo/Assets/Scripts/Player/Control/PlayerHitCheck.cs
@@ -5,9 +5,24 @@ public class PlayerHitCheck : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer spriteRenderer;
     private CharacterManager manager;
+    [Header("Hit FX")]
+    [SerializeField] private float hitShakeDuration = 0.08f;
+    [SerializeField] private float hitShakeAmplitude = 0.2f;
+    [SerializeField] private float hitShakeFrequency = 25f;
+    [SerializeField] private float hitZoomAmount = 0.5f;
+    [SerializeField] private float hitZoomInDuration = 0.04f;
+    [SerializeField] private float hitZoomHoldDuration = 0.05f;
+    [SerializeField] private float hitZoomOutDuration = 0.08f;
+    [Header("Hit Slow")]
+    [SerializeField] private float hitSlowScale = 0.2f;
+    [SerializeField] private float hitSlowDuration = 0.06f;
+    private CameraFollow2D camFollow;
+    private CameraShake camShake;
+    private CameraZoom camZoom;
     void Start()
     {
         manager = GameManager.Instance.Char;
+        CacheCameraFx();
     }
 
     [Header("Invincibility")]
@@ -50,6 +65,8 @@ public class PlayerHitCheck : MonoBehaviour
             Die();
             return;
         }
+        ApplyHitSlow();
+        ApplyHitFx();
         StartCoroutine(NoDamage());
     }
 
@@ -68,5 +85,57 @@ public class PlayerHitCheck : MonoBehaviour
     void Die()
     {
         Debug.Log("Player Dead");
+    }
+
+    private void ApplyHitFx()
+    {
+        ApplyHitFx(
+            hitShakeDuration,
+            hitShakeAmplitude,
+            hitShakeFrequency,
+            hitZoomAmount,
+            hitZoomInDuration,
+            hitZoomHoldDuration,
+            hitZoomOutDuration
+        );
+    }
+
+    private void ApplyHitFx(
+        float shakeDuration,
+        float shakeAmplitude,
+        float shakeFrequency,
+        float zoomAmount,
+        float zoomInDuration,
+        float zoomHoldDuration,
+        float zoomOutDuration
+    )
+    {
+        if (camFollow != null)
+        {
+            camFollow.Shake(shakeDuration, shakeAmplitude, shakeFrequency);
+            camFollow.ZoomInHoldOut(zoomAmount, zoomInDuration, zoomHoldDuration, zoomOutDuration);
+            return;
+        }
+
+        if (camShake != null) camShake.Shake(shakeDuration, shakeAmplitude, shakeFrequency);
+        if (camZoom != null) camZoom.ZoomInHoldOut(zoomAmount, zoomInDuration, zoomHoldDuration, zoomOutDuration);
+    }
+
+    private void ApplyHitSlow()
+    {
+        var timeManager = GameManager.Instance != null ? GameManager.Instance.TimeManager : null;
+        if (timeManager == null) return;
+
+        if (hitSlowDuration > 0f) timeManager.EnterBulletTime(hitSlowScale, hitSlowDuration);
+        else timeManager.EnterBulletTime(hitSlowScale);
+    }
+
+    private void CacheCameraFx()
+    {
+        var cam = Camera.main;
+        if (cam == null) return;
+        camFollow = cam.GetComponent<CameraFollow2D>();
+        camShake = cam.GetComponent<CameraShake>();
+        camZoom = cam.GetComponent<CameraZoom>();
     }
 }
