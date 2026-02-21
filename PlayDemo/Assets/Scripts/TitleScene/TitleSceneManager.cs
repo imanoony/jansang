664883edf017d3253fe14631df33280b38dd51d1
Minseo Jeon <minseo4565@snu.ac.kr>
2026-02-21 19:34:27 +0900

    [feat] tmp titlescene
    # Conflicts:
    #	PlayDemo/Assets/InputSystem_Actions.inputactions
    #	PlayDemo/Assets/Sprites/Platform.meta
    #	PlayDemo/Assets/Sprites/Platform/HalfTile.png.meta
    #	PlayDemo/ProjectSettings/EditorBuildSettings.asset

---
+++ PlayDemo/Assets/Scripts/TitleScene/TitleSceneManager.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleSceneManager : MonoBehaviour
+{
+    public Image fadePanel;
+    public Image[] backgrounds;
+
+    public InputAction anykeyAction;
+
+    
+    private bool changeStarted = false;
+    public string targetSceneName = "Level0";
+
+    public float changeTime = 2f;
+    public float backgroundMoveTime = 5f;
+
+    public Image loadingBar;
+    
+    private void Start()
+    {
+        anykeyAction = InputSystem.actions.FindActionMap("Player").FindAction("AnyKey");
+        anykeyAction.Enable();
+        anykeyAction.started += GoToStage;
+    }
+
+    private void GoToStage(InputAction.CallbackContext context)
+    {
+        if (!changeStarted)
+        {
+            changeStarted = true;
+            StartCoroutine(ChangeScene());
+        }
+    }
+
+    IEnumerator ChangeScene()
+    {
+        float elapsed = 0;
+        Color c = Color.black;
+        c.a = 0;
+        while (true)
+        {
+            for (int i = 0; i < backgrounds.Length; i++)
+            {
+                var b = backgrounds[i];
+                b.rectTransform.position += Vector3.left * (elapsed * Time.deltaTime * (backgroundMoveTime + i * 50));
+            }
+            c.a  = elapsed / changeTime;
+            fadePanel.color = c;
+            elapsed += Time.deltaTime;
+            yield return null;
+
+            if (elapsed >= changeTime)
+            {
+                break;
+            }
+        }
+        
+        var sceneLoad = SceneManager.LoadSceneAsync(targetSceneName, LoadSceneMode.Single);
+        if (sceneLoad == null)
+        {
+            #if UNITY_EDITOR
+            
+            Debug.LogWarning("Scene not found");
+            yield break;
+            
+            #endif
+        }
+        
+        
+        loadingBar.gameObject.SetActive(true);
+        while (sceneLoad.progress < 0.9f)
+        {
+            loadingBar.fillAmount = sceneLoad.progress / 0.9f;
+        }
+    }
+}
